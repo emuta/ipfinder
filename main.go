03828905e950f1d65a55a1b5b8f2697fc725295e
emuta <emuta@mail.com>
2019-02-24 20:37:59 +0800

    use logrus as logger

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
     "flag"
-    "log"
     "net"
     "net/http"
     "encoding/json"
-
+    
+    "github.com/sirupsen/logrus"
     "github.com/ipipdotnet/datx-go"
 )
 
@@ -14,10 +14,19 @@ var addr   = flag.String("addr", ":80", "service listen address")
 var dbfile = flag.String("dbfile", "./17monipdb.datx", "db file of 17monipdb.datx path")
 
 var city *datx.City
+var log *logrus.Logger
 
 func init() {
     flag.Parse()
 
+    // initailize logger
+    log = logrus.New()
+    log.SetFormatter(&logrus.TextFormatter{
+        FullTimestamp:   true,
+        TimestampFormat: "2006-01-02 15:04:05",
+    })
+
+    // load dbfile
     var err error
     if city, err = datx.NewCity(*dbfile); err != nil {
         log.Fatal(err)
@@ -38,20 +47,23 @@ func Handler(w http.ResponseWriter, r *http.Request) {
         ip, _, _ = net.SplitHostPort(r.RemoteAddr)
     }
 
-    // loc, err := city.Find(ip)
     loc, err := city.FindLocation(ip)
     if err != nil {
         w.WriteHeader(404)
         w.Write([]byte(err.Error()))
+        log.WithError(err).WithField("ip", ip).Error(err)
         return
     }
+
     w.Header().Set("Content-Type", "application/json;charset=UTF-8")
     l := &Location{loc.Country, loc.Province, loc.City}
     json.NewEncoder(w).Encode(l)
 
-    go func() {
-        log.Printf("[%s] %s, %#v", r.RemoteAddr, r.URL, l)
-        }()
+    log.WithFields(logrus.Fields{
+        "remote": r.RemoteAddr, 
+        "country": l.Country,
+        "province": l.Province,
+        "city": l.City}).Info(r.URL)
 }
 
 func main() {
